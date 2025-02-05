110aa9b825ea05749dd2db0dac12737c98b1e2f2
taeyeon5211 <jty84373786@gmail.com>
2025-02-05 15:33:27 +0900

    JTY_0205_01

---
+++ src/test02/Dog.java
@@ -0,0 +1,14 @@
+package test02;
+
+public class Dog extends Animal{
+
+    public Dog(int speed) {
+        super(speed);
+    }
+
+    @Override
+    void run(int hours) {
+        setDistance((double) (getSpeed() * hours) / 2);
+        System.out.println("개의 이동거리 = " + getDistance());
+    }
+}
