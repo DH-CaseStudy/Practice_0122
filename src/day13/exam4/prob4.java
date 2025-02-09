7cab9962181b9ebcb12cee4433d99a6f7b5e15d5
Donghwi <cksael@gmail.com>
2025-02-09 22:30:34 +0900

    폴더분리

--- src/day13/exam4/prob4.java
+++
@@ -1,9 +0,0 @@
-package day13.exam4;
-
-public class prob4 {
-    public static void main(String[] args) {
-        Student student = new Student(100000, "javaprogram", "이동휘");
-        student.calcReturnFee();
-        student.print();
-    }
-}
