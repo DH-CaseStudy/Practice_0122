c626417016fc36e2b8e2a3de3b0a1e3c77d63a8c
donghwi <cksael@gmail.com>
2025-03-08 18:05:00 +0900

    상속관계 추가

--- src/MainSystem/model/EmployeeManager.java
+++ src/MainSystem/model/EmployeeManager.java
@@ -5,7 +5,7 @@ import MainSystem.model.dao.EmployeeDBIO;
 import java.sql.SQLException;
 import java.util.List;
 
-public class EmployeeManager {
+public class EmployeeManager extends EmployeeDBIO {
     private static EmployeeManager instance;
     private static EmployeeDBIO employeeDBIO;
 
@@ -18,6 +18,7 @@ public class EmployeeManager {
         return instance;
     }
 
+    @Override
     public boolean addEmployee(Employee employee) {
         if (employee == null || employee.getEno().isEmpty()) {
             System.out.println("올바른 직원 정보가 아닙니다.");
@@ -27,6 +28,7 @@ public class EmployeeManager {
         return employeeDBIO.addEmployee(employee);
     }
 
+    @Override
     public boolean updateEmployee(Employee employee) throws SQLException {
         if (employee == null || employee.getEno().isEmpty()) {
             System.out.println("올바른 직원 정보가 아닙니다.");
@@ -36,26 +38,32 @@ public class EmployeeManager {
         return employeeDBIO.updateEmployee(employee);
     }
 
+    @Override
     public Employee getEmployeeById(String eno) {
         return employeeDBIO.getEmployeeById(eno);
     }
 
+    @Override
     public List<Employee> getAllEmployees() {
         return employeeDBIO.getAllEmployees();
     }
 
+    @Override
     public List<Employee> searchEmployeesByName(String name) {
         return employeeDBIO.searchEmployeesByName(name);
     }
 
+    @Override
     public List<Employee> searchEmployeesByRole(String role) {
         return employeeDBIO.searchEmployeesByRole(role);
     }
 
+    @Override
     public List<Employee> getUnassignedSecretaries() {
         return employeeDBIO.getUnassignedSecretaries();
     }
 
+    @Override
     public boolean selectUpdateEmployee(Employee employee){
         return employeeDBIO.selectUpdateEmployee(employee);
     }
